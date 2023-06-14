fb6247266aa75d59dd1e6e92cd3d6ffa0aea40ec
ericafamorim <2002.ericafalcao@gmail.com>
2023-06-13 23:55:35 -0300

    Models criado

--- apps/porte_animal/models.py
+++ apps/porte_animal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from animais.models import Animais
 
-# Create your models here.
+class PorteAnimal(models.Model):
+    altura = models.FloatField("altura animal")
+    peso = models.FloatField("peso animal")
+    animal = models.ForeignKey(Animais, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'PorteAnimal'
+        verbose_name_plural = 'PorteAnimais'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.altura}-{self.peso}"
