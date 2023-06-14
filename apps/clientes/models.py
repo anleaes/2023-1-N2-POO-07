3a4072875fba960f2749395fbdd2454340ff6c1b
ericafamorim <2002.ericafalcao@gmail.com>
2023-06-13 23:01:05 -0300

    Alteração na MODEL

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Clientes(models.Model):
     nome = models.CharField("Nome", max_length=20)
     sobrenome = models.TextField("Sobrenome", max_length=35)
-    documento = models.IntegerField("CPF", max_length=11)
+    documento = models.IntegerField("CPF")
     idade = models.IntegerField("Idade", default=0)
-    data_nascimento = models.DateTime("Data de nascimento")
+    data_nascimento = models.DateTimeField("Data de nascimento")
 
     class Meta:
         verbose_name = 'Cliente'
