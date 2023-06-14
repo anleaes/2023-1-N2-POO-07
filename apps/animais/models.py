719a9ee69848840ed7872595cb056dad4eabc59d
ericafamorim <2002.ericafalcao@gmail.com>
2023-06-13 23:29:05 -0300

    Criação da MODEL

--- apps/animais/models.py
+++ apps/animais/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Animais(models.Model):
+    especie = models.CharField("Espécie")
+    name = models.CharField("Nome")
+    raca = models.CharField("Raça")
+    idade = models.IntegerField("Idade")
+
+    
+    class Meta:
+        verbose_name = 'Animal'
+        verbose_name_plural = 'Animais'
+        ordering =['id']
+
+    def __str__(self):
+         return f"{self.especie}-{self.nome}-{self.idade}"
